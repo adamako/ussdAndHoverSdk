d20d110633100e5f97a02afd9d2b985202100daf
Adama Ko <adamako34@hotmail.com>
2020-11-13 16:02:15 +0000

    get message

--- android/app/src/main/kotlin/com/example/ussd_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ussd_sample/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.ussd_sample
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
-import android.widget.Toast
 import com.hover.sdk.api.Hover
 import com.hover.sdk.api.HoverParameters
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL: String= "kikoba.co.tz/hover"
@@ -17,6 +19,7 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
 ////          val arguments: Map<String, Any> = call.arguments()
 //            val phoneNumber: String= arguments["phoneNumber"] as String
@@ -29,24 +32,21 @@ class MainActivity: FlutterActivity() {
 //                val response: String= "Envoyé"
 //                result.success(response)
 //            }
+
             when(call.method){
                 "checkMoney" -> {
+                    val response: String =getMessage()
                     checkMoney()
-                    val response: String= "Envoyé"
-
                     result.success(response)
-                }
-                "getMessage"->{
-                    getMessage()
-                    result.success(getMessage())
                 }
             }
         }
     }
 
-    private fun getMessage(): String {
+    fun getMessage(): String {
         return message
     }
+
 
 
     private fun SendMoney(phoneNumber: String, amount: String){
@@ -78,20 +78,27 @@ class MainActivity: FlutterActivity() {
         val i:Intent =HoverParameters.Builder(this)
                 .request("288a7dbd")
                 .buildIntent()
+
         startActivityForResult(i, 0)
+
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
-            val sessionTextArr = data.getStringArrayExtra("session_messages")
-            val uuid = data.getStringExtra("uuid")
-            for (me in sessionTextArr.indices){
-                message+=me
+            val sessionTextArr = data?.getStringArrayExtra("session_messages")
+            val uuid = data?.getStringExtra("uuid")
+            if (sessionTextArr != null) {
+                for (me in sessionTextArr.indices){
+                    message+=me
+                }
             }
 
         } else if (requestCode == 0 && resultCode == Activity.RESULT_CANCELED) {
-            Log.d("MainActivity", "Error: " + data.getStringExtra("error"))
+            if (data != null) {
+                Log.d("MainActivity", "Error: " + data.getStringExtra("error"))
+            }
         }
     }
 }
